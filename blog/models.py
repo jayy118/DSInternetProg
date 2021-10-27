4304fb2cde4a223aa3a09d32df701b86b4572acb
=Jae-Hyun Lee <hyun918@duksung.ac.kr>
2021-10-27 16:20:44 +0900

    포스트 목록/상세 페이지에 작성자 출력하기

--- blog/models.py
+++ blog/models.py
@@ -1,5 +1,5 @@
 import os.path
-
+from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
@@ -14,10 +14,11 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    #author
+    author = models.ForeignKey(User,null=True,
+                               on_delete=models.SET_NULL)
 
     def __str__(self):
-        return f'[{self.pk}]{self.title}'
+        return f'[{self.pk}]{self.title} :: {self.author}'
 
     def get_absolute_url(self):
         return f'/blog/{self.pk}'
