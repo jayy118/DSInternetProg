77c39435504062d3f08547ef24e518e742f7d60e
=Jae-Hyun Lee <hyun918@duksung.ac.kr>
2021-11-03 16:34:11 +0900

    포스트 목록/상세 페이지에 태그 기능 추가 및 테그페이지 만들기

--- blog/models.py
+++ blog/models.py
@@ -3,6 +3,17 @@ from django.contrib.auth.models import User
 from django.db import models
 
 # Create your models here.
+
+class Tag(models.Model):
+    name = models.CharField(max_length=50, unique=True)
+    slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return f'/blog/tag/{self.slug}'
+
 class Category(models.Model):
     name = models.CharField(max_length=50, unique=True)
     slug = models.SlugField(max_length=200, unique=True, allow_unicode=True)
@@ -27,10 +38,12 @@ class Post(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
 
-    author = models.ForeignKey(User,null=True, blank=True,
+    author = models.ForeignKey(User, null=True, blank=True,
                                on_delete=models.SET_NULL)
 
-    category = models.ForeignKey(Category, null=True, on_delete=models.SET_NULL)
+    category = models.ForeignKey(Category, null=True, blank=True, on_delete=models.SET_NULL)
+
+    tags = models.ManyToManyField(Tag, blank=True)
 
     def __str__(self):
         return f'[{self.pk}]{self.title} :: {self.author}'
--- blog/urls.py
+++ blog/urls.py
@@ -5,6 +5,7 @@ urlpatterns = [
 #    path('<int:pk>/', views.single_post_page),
 #    path('', views.index),
 
+    path('tag/<str:slug>', views.tag_page),
     path('category/<str:slug>', views.category_page),
     path('<int:pk>/', views.PostDetail.as_view()),
     path('', views.PostList.as_view()),
